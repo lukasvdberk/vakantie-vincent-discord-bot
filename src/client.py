4b99c3b7bb59d52618b48aa0c80c453c5bb9c60d
Lukas van den Berk <lukasvdberk@gmail.com>
2020-07-15 14:16:05 +0200

    Added send video on command !ikwilvincent

--- src/client.py
+++ src/client.py
@@ -1,8 +1,14 @@
 import discord
-from video import send_random_video
+from video import send_random_video, get_random_video
 
 
 class VakantieVincentDiscordClient(discord.Client):
+    bot_command = "!ikwilvincent"
+
     async def on_ready(self):
         await send_random_video(self)
-        print('Logged on as {0}!'.format(self.user))
+        print("Logged on as {0}!".format(self.user))
+
+    async def on_message(self, message):
+        if message.content == self.bot_command:
+            await message.channel.send(get_random_video())
